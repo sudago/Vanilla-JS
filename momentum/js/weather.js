c2d444e3d726493b08d02063aab77eca1f30a342
sudago <sudago22@gmail.com>
2022-03-09 21:19:31 +0900

    update #8 end

---
+++ momentum/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "4b260bb8345a264575fac57f6e902bc0";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then((data)  => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+        city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No Weather for you.");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
