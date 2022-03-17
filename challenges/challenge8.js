baac59f7d5b72cb2a152c91adadddacc515ee67a
sudago <sudago22@gmail.com>
2022-03-17 12:33:03 +0900

    bg color change

---
+++ challenges/challenge8.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector("button");
+const body = document.querySelector("body")
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
+  
+  function colorChange() {
+    const randomColors1 = colors[Math.floor(Math.random()* colors.length)];
+    const randomColors2 = colors[Math.floor(Math.random()* colors.length)];
+     body.style.background = `linear-gradient(to right, ${randomColors1}, ${randomColors2})`;
+}
+
+btn.addEventListener("click", colorChange);
